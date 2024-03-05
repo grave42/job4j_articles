aac72a9dd7fb5dabdcee8a1798b117b34fd53e27
filipp.kucherov <filipp.kucerov@r1.team>
2024-03-05 21:40:22 +0700

    3. GC. Out Of Mem. Тестовое задание. [#504790] fix

--- src/main/java/ru/job4j/articles/model/Article.java
+++ src/main/java/ru/job4j/articles/model/Article.java
@@ -35,8 +35,12 @@ public class Article {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Article article = (Article) o;
         return id == article.id && Objects.equals(text, article.text);
     }
